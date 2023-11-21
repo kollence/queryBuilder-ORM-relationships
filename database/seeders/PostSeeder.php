97f2031c89b6bb54e2cde80264250ca0a83b4ba4
kole <misterija@gmail.com>
2023-11-21 18:46:08 +0100

    Make post factory not from JSON

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,14 +14,14 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        $json = File::get("database/json/posts.json");
-        $decodeToArray = json_decode($json);
-        $posts = collect($decodeToArray);
+        // $json = File::get("database/json/posts.json");
+        // $decodeToArray = json_decode($json);
+        // $posts = collect($decodeToArray);
         // dd($posts);
 
 
 
-        // Post::factory()->count(10)->create();
+        Post::factory()->count(10)->create();
         // $posts = collect([
         //     [
         //         'title' => 'Post 1',
@@ -41,7 +41,8 @@ class PostSeeder extends Seeder
         //     ]
         // ]);
 
-        $posts->each(fn ($post) => Post::create(['title'=>$post->title, 'slug'=>$post->slug, 'content'=>$post->content, 'excerpt'=>$post->excerpt, 'is_published'=>$post->is_published, 'min_to_read'=>$post->min_to_read]));
+        // $posts->each(fn ($post) => Post::create(['user_id'=> $post->user_id, 'title'=>$post->title, 'slug'=>$post->slug, 'content'=>$post->content, 'excerpt'=>$post->excerpt, 'is_published'=>$post->is_published, 'min_to_read'=>$post->min_to_read]));
+
         // Post::create([
         //     'title' => 'Post 1',
         //     'slug' => 'post-1',
