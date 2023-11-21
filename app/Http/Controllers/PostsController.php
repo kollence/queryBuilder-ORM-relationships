d4c0d502b6cb3dd5b3d7c9fb26dfc5fcf1bcbeed
kole <misterija@gmail.com>
2023-11-21 22:14:59 +0100

    insert() data[] will be saved to database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,29 +12,19 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //pluck() will take just values from given column and return it as array
        $posts = DB::table('posts')
-       ->pluck('title');
-
+       // insert data in table
+        ->insert([
+            'user_id' => 1,
+            'title' => 'Post 1',
+            'slug' => 'post-1',
+            'content' => 'Post 1 Body',
+            'excerpt' => 'Post 1 Body',
+            'is_published' => false,
+            'min_to_read' => 2,
+        ]);
+        //will return true on stored
        dd($posts);
-       //result
-       /**
-        * Illuminate\Support\Collection {#1342 ▼ // app\Http\Controllers\PostsController.php:19
-        *   #items: array:10 [▼
-        *        0 => "Aliquid quis dicta expedita reprehenderit maiores."
-        *        1 => "Dolor enim amet sapiente."
-        *        2 => "Est quidem recusandae in sed maxime enim."
-        *        3 => "Occaecati est animi ullam eligendi veritatis."
-        *        4 => "Quasi aspernatur quia sed quaerat quidem."
-        *        5 => "Reprehenderit fugit nesciunt et et harum alias."
-        *        6 => "Sequi est consequuntur error unde."
-        *        7 => "Sint tempore rerum ipsum deleniti."
-        *        8 => "Vel omnis saepe suscipit et facilis voluptatum."
-        *        9 => "Velit omnis aut corrupti sed ad doloremque."
-        *    ]
-        */
-
-       
     }
 
     /**
