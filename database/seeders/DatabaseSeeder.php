2378396a7173592fc2c64c11e3108afd31c46585
kole <misterija@gmail.com>
2023-11-21 18:45:44 +0100

    remove UserSeeder not needed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             PostSeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
