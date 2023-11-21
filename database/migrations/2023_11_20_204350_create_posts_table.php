394c4eccb017de00476265389f97a09e539bf152
kole <misterija@gmail.com>
2023-11-21 18:18:46 +0100

    `migrate:refresh` & look table by `db:table posts`

--- database/migrations/2023_11_20_204350_create_posts_table.php
+++ database/migrations/2023_11_20_204350_create_posts_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
 
             $table->id()->startingValue(100);
-
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            
             $table->string('title');
             $table->string('slug');
             $table->longText('content');
