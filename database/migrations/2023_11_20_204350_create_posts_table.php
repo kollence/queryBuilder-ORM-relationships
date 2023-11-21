ce7e1403ee47a215801f8799c813276410f5a3b0
kole <misterija@gmail.com>
2023-11-21 18:04:01 +0100

    foreign key set to be on colum user_id

--- database/migrations/2023_11_20_204350_create_posts_table.php
+++ database/migrations/2023_11_20_204350_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
 
             $table->id()->startingValue(100);
             $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id');
 
             $table->string('title');
             $table->string('slug');
