c1c87f6cc8ddb655878c22372a49961b2d50a231
kole <misterija@gmail.com>
2023-11-21 18:05:03 +0100

    references() to id on() table users

--- database/migrations/2023_11_20_204350_create_posts_table.php
+++ database/migrations/2023_11_20_204350_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 
             $table->id()->startingValue(100);
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
 
             $table->string('title');
             $table->string('slug');
