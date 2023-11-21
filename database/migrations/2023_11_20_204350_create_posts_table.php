c20d8d174e60842b3e7169e4d61fbccfcf73bd20
kole <misterija@gmail.com>
2023-11-21 18:06:04 +0100

    cascadeOnDelete deletes post that points to deleted user_id

--- database/migrations/2023_11_20_204350_create_posts_table.php
+++ database/migrations/2023_11_20_204350_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 
             $table->id()->startingValue(100);
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
 
             $table->string('title');
             $table->string('slug');
