1cedc9aa89d4c371d69b4876408c4831dfab4ba2
kole <misterija@gmail.com>
2023-11-21 18:01:22 +0100

    unsigned integer means that the column does not allow negative values

--- database/migrations/2023_11_20_204350_create_posts_table.php
+++ database/migrations/2023_11_20_204350_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
 
             $table->id()->startingValue(100);
+            $table->unsignedBigInteger('user_id');
 
             $table->string('title');
             $table->string('slug');
