7fbc2e0a28e44d5f9a2ec44c7230b0c7dd891a7f
kole <misterija@gmail.com>
2023-11-22 17:52:09 +0100

    upsert insert new record or update if specified fields not changed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,34 +12,36 @@ class PostsController extends Controller
      */
     public function index()
     {
-       $posts = DB::table('posts')
-       // insertOrIgnore() method that allows you to insert data into a DB table only if the row does not already exist.
-        ->insertOrIgnore([
+        //The upsert method will insert records that do not exist and update the records that already exist with new values that you may specify.
+        $posts = DB::table('posts')
+            ->upsert(
             [
-                'user_id' => 2,
-                'title' => 'Post 3 insertOrIgnore',
-                'slug' => 'post-3-insertOrIgnore',
-                'content' => 'first from 2 insertOrIgnore ',
-                'excerpt' => 'first from 2 insertOrIgnore ',
-                'is_published' => false,
-                'min_to_read' => 6,
-                'created_at'    => now(),
-                'updated_at'    => now(),
-            ],
-            [
-                'user_id' => 3,
-                'title' => 'Post 6 insertOrIgnore',
-                'slug' => 'post-6-insertOrIgnore',
-                'content' => 'second from 2 insertOrIgnore ',
-                'excerpt' => 'second from 2 insertOrIgnore ',
-                'is_published' => false,
-                'min_to_read' => 4,
-                'created_at'    => now(),
-                'updated_at'    => now(),
-            ]
-        ]);
-        //will return true on stored
-       dd($posts);
+                [
+                    'user_id' => 2,
+                    'title' => 'X', // No change // Title AND/OR slug not changed so it will update existing record 
+                    'slug' => 'x',  // No change // Slug AND/OR title not changed so it will update existing record 
+                    'content' => 'Update se / Updating fields', // just this will be updated because its changed but value of title & slug are same & unique
+                    'excerpt' => 'first from / Updating fields',
+                    'is_published' => false,
+                    'min_to_read' => 6,
+                ],
+                [
+                    'user_id' => 3,
+                    'title' => 'Y 2', // Changed // Title AND/OR slug are changed from Y to Y 2 so it will add new record 
+                    'slug' => 'y-2',  // Changed // Slug AND/OR title are changed from y to y-2 so it will add new record
+                    'content' => 'Create se / Created new record', // Everything will be created as new record because [Title, Slug] are not unique anymore
+                    'excerpt' => 'first from / Created new record',
+                    'is_published' => false,
+                    'min_to_read' => 6,
+                ]
+            ], 
+            // second argument lists the column(s) that uniquely identify records within the associated table
+            ['title', 'slug'] // (Act like its unique identifier)
+            // third and final argument is an array of column(s) that should be updated if a matching record already exists in the database
+            //['user_id', 'content', 'excerpt', 'is_published', 'min_to_read']
+        );
+        //will return 0 if nothing to add or update or number of records updated
+        dd($posts);
     }
 
     /**
