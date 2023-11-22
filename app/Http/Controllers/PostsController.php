891943fd0c330d2be9ad15516c3cb77fe400b148
kole <misterija@gmail.com>
2023-11-22 19:10:09 +0100

    insertGetId will return id of newly inserted data(single record)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,36 +12,19 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //The upsert method will insert records that do not exist and update the records that already exist with new values that you may specify.
+        //If the table has an auto-incrementing id, use the insertGetId method to insert a record and then retrieve the ID:
         $posts = DB::table('posts')
-            ->upsert(
-            [
-                [
-                    'user_id' => 2,
-                    'title' => 'X', 
-                    'slug' => 'x',
-                    'content' => 'Update se / Updating fields',
-                    'excerpt' => 'first from / Updating fields',
+            ->insertGetId([
+                    'user_id' => 10,
+                    'title' => 'Insert Get Id', 
+                    'slug' => 'insert-get-id',
+                    'content' => 'Inertuje novi rekord i vraca njegov TEK KREIRAN id',
+                    'excerpt' => 'first from / Inertuje novi rekord i vraca njegov TEK KREIRAN id',
                     'is_published' => false,
                     'min_to_read' => 6,
-                ],
-                [
-                    'user_id' => 4,
-                    'title' => 'Y 4',
-                    'slug' => 'y-C',
-                    'content' => 'Up? eate se / Created new record',
-                    'excerpt' => 'Apdejt from / Created new record',
-                    'is_published' => false,
-                    'min_to_read' => 6,
-                ]
-            ], 
-            // ALREADY KNOWS THAT THIS FIELD NEED TO BE UNIQUE, SO WE ACT LIKE PRIMARY KEY
-            // IT KNOWS BECAUSE ITS SET IN MIGRATION FILE AS $table->unique(['title', 'slug'])
-            ['title, slug'], // (Act like its unique identifier)
-            // third and final argument is an array of column(s) that should be updated if a matching record already exists in the database
-            // ['title', 'slug', 'excerpt']
+            ]
         );
-        //will return 0 if nothing to add or update or number of records updated
+        //will return ID of inserted new record
         dd($posts);
     }
 
