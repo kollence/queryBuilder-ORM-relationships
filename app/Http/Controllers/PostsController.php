25e63c2e3e165bec80a5ca50ce2edfbe39c276df
kole <misterija@gmail.com>
2023-11-24 19:23:47 +0100

    lazy() retrieve large num of records as LazyCollection

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,25 +11,18 @@ class PostsController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {   //GOOD FOR PROCESS DATA RETURNED FROM QUERY 
-        //useful for performing calculation, filtering data, or transforming data in different format
-        
-        // chunk() Chunking large set of data. Retrieves data in smaller & more manageable set.
-        // chunking second param callback function can be use for ANY PROCESSING OF DATA THAT YOU WANT
-        // BROWSER will load all time till the last chunk of data is not processed
-        // BETTER THEN PAGINATE = (paginate retrieve everything from database and then chunk result in pages)
-        // BETTER FOR PERFORMANCE
+    {   
+        // lazy() LAZY LOAD DATA is used to retrieve a large number of records WITHOUT OVERWHELMING THE SERVER MEMORY
+        // return LazyCollection and not regular collection
+        // LazyCollection class in Laravel is designed for working with large datasets in a memory-efficient manner.
+        // LazyCollection When you iterate over the lazy collection, the underlying query is executed in chunks, loading only a portion of the results into memory at a time
         $posts = DB::table('posts')
         ->orderByDesc('id')
-        ->chunk(100, function($posts) { // param 1: number of chunked rows. param 2: callback function receive num of chunked rows
-                                      //once call back are finished processing it will move to next coming chunked set
-                                      //running till last chunk is loaded
-            foreach($posts as $post){
-                dump($post); //DUMP() a post so you could watch process and not killing it
-            }
-        });
+        ->lazy();
 
-        
+
+        dd($posts); // return Illuminate\Support\LazyCollection
+                    // with default chunk of 1000 records (->source->use->$chunkSize = 1000)
     }
 
     /**
