d8a063424b5f722456c86c5f33aeb438f9193187
kole <misterija@gmail.com>
2023-11-26 17:37:19 +0100

    orWhereFullText() chained|OR WHERE MATCH() AGAINST(text, FLAG)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,16 +13,24 @@ class PostsController extends Controller
     
     public function index()
     {   // 1 word
+        $word = 'Quidem';
         // 2 paragraph 
-        $fullText = '1 Quidem, 2 nemo veritatis possimus laudantium odit';
-        // whereFullText() [Full Text Indexes] is made for searching words, phrases, and sentences in text columns.
+        $paragraph = 'nemo odit';
+        // orWhereFullText() that can be chained to whereFullText() [Full Text Indexes] is made for searching words, phrases, and sentences in text columns.
         // Best for searching a large amount of text. Maybe for Blog or Search Engine
-        // Adds full text to WHERE clause for column that have [FULL TEXT INDEXES]
+        // Adds full text to OR WHERE clause for column that have [FULL TEXT INDEXES]
         $posts = DB::table('posts')
-        ->whereFullText('content', $fullText) // we set in migration fullText() for `content` column
+        ->whereFullText('content', $word) // check for $word in `content` column
+        ->OrWhereFullText('content', $paragraph) // check for $paragraph in `content` column
         ->get();
 
-        dd($posts); // return collection whit anything that matches $fullText
+        // $posts->toSql();                                          flags:IN BOOLEAN MODE                                               flags:IN BOOLEAN MODE
+        // SELECT * FROM `posts` WHERE MATCH (`content`) AGAINST ('Quidem' IN NATURAL LANGUAGE MODE) OR MATCH (`content`) AGAINST ('nemo odit' IN NATURAL LANGUAGE MODE)
+
+        // [NATURAL LANGUAGE MODE] is feature of full text search
+        // that allows users to search for words and phrases in natural language syntax
+        // that ignores word order and word capitalization.
+        dd($posts); // return collection whit anything that matches WHERE $word OR WHERE $paragraph in column `content`
     }
 
     /**
