cb85c32b7f97a74e16b9d639f5a82a8ae9211349
kole <misterija@gmail.com>
2023-11-27 16:28:45 +0100

    protected $table overwrite table name

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    // OVERWRITE  $table name that will refer to the table in the database
+    protected $table = 'users'; // Post::all() return users
 }
