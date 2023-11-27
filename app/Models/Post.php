9e89d7551181d3cf6ab3cc2d8e3fa0246a1d0f88
kole <misterija@gmail.com>
2023-11-27 18:28:54 +0100

    BUILDING QUERIES setup

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,8 +10,5 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    // CONNECTION PROPERTY will be set which connection with database will be used for this MODEL
-    // Change the DB interacting with a particular MODEL
-    protected $connection = 'sqlite'; // String name should be equal to the configuration file config/database.php                
+              
 }
