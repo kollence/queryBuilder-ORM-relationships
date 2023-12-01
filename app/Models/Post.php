367d98f72112ab96cb995eb09a8d6ea9810c6cd5
kole <misterija@gmail.com>
2023-12-01 20:03:23 +0100

    addGlobalScope() example

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\PublishedOnThirtyDaysScope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -13,6 +14,11 @@ class Post extends Model
     use HasFactory, SoftDeletes, Prunable;
 
     protected $guarded = [];
+
+    protected static function booted() // life cycle 
+    {   // addGlobalScope 
+        static::addGlobalScope(new PublishedOnThirtyDaysScope);
+    }
 
     public function prunable(): Builder
     {// using Prunable Trait in Model is grate way to remove outdated data from db
