dbf78b52b624ac2f9fdf9efac0a543d36627c414
kole <misterija@gmail.com>
2023-12-02 16:29:52 +0100

    DYNAMIC SCOPES specify condition that you want to filter by

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,11 +29,18 @@ class Post extends Model
     {
         $query->where('is_published', true);
     }
-
+    // Local Scope = write reusable query on model
     public function scopeWithUser(Builder $query): void
     {
         $query->join('users', 'posts.user_id', '=', 'users.id')
         ->select('posts.*', 'users.name as author_name');
     }
-              
+    // Dynamic scopes allow you to specify the condition that you want to filter by
+    // Flexibility to change the conditions at run time without need to modify your code
+    public function scopePublishedByUser(Builder $query, $userId, $bool): void
+    {  //Post::publishedByUser($userId,$bool)->withUser()->get();
+        $query->where('user_id', $userId)
+        ->where('is_published', $bool);
+    }
+
 }
