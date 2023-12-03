21a42eb978977f6e2a6ac9bc3265caf171b2562e
kole <misterija@gmail.com>
2023-12-03 18:44:00 +0100

    OtO| blade index with create form & routes -C

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,12 @@ Route::resource('/posts', PostsController::class);
 Route::get('removeAllFromSoftDeleted', [PostsController::class, 'removeAllFromSoftDeleted']);
 Route::get('removeSingleFromSoftDeleted', [PostsController::class, 'removeSingleFromSoftDeleted'])->name('posts.restore');
 Route::get('replicatePost', [PostsController::class, 'replicatePost'])->name('posts.replicate');
+
+Route::controller(UsersController::class)->group(function () {
+    Route::get('users', 'index')->name('users.index');
+    Route::post('users/{user}', 'storeContact')->name('users.storeContact');
+    Route::get('users/{user}', 'show');
+    Route::post('users', 'store');
+    Route::put('users/{user}', 'update');
+    Route::delete('users/{user}', 'destroy');
+});
